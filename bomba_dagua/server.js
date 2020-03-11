d71155ca5062139449fde6e72b23ded76b09eb8b
Mrs Solaris <mrssolaris.dev@gmail.com>
2020-03-10 21:50:15 -0300

    Upando cliente e servidor temporarios da bomba dagua

---
+++ bomba_dagua/server.js
@@ -0,0 +1,39 @@
+// We'll be using the https://github.com/theturtle32/WebSocket-Node
+// WebSocket implementation
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+  // process HTTP request. 
+});
+server.listen(6790, function() { 
+  console.warn(`Escutando`);
+});
+
+// create the server
+wsServer = new WebSocketServer({
+  httpServer: server
+});
+
+// WebSocket server
+wsServer.on('request', function(request) {
+  var connection = request.accept(null, request.origin);
+
+  setInterval(() => { 
+    connection.send(`Enviando dados da bomba d'agua`);
+  }, 6000); //manda uma mensagem para o cliente de 6 em 6 segundos.
+  
+  //Listener de mensagem do lado do cliente.
+  connection.on('message', function(mensagem) {
+      // Process WebSocket message
+      console.log("Message from client");
+      console.log(mensagem);
+  });
+
+  //Fecha a conexão.
+  connection.on('close', function(conexao) {
+    // Connection closes
+    console.log(`Conexao com ${conexao} encerrada`);
+    connection.close();
+  });
+});
